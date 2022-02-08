0a2caf1c93e77f7fbdbac9479367e50e6cddf9ce
wddcheng <du.cheng@fmr.com>
2022-02-08 15:00:07 -0500

    made changes
    Signed-off-by: wddcheng <du.cheng@fmr.com>

--- tests/test_reco_diversity.py
+++ tests/test_reco_diversity.py
@@ -2,12 +2,12 @@
 # Copyright FMR LLC <opensource@fidelity.com>
 # SPDX-License-Identifier: Apache-2.0
 
-import unittest, random
+import unittest, random, warnings
 
 import pandas as pd
 import numpy as np
 
-from jurity.recommenders import DiversityRecoMetrics, RankingRecoMetrics
+from jurity.recommenders import DiversityRecoMetrics
 from jurity.utils import Constants
 
 
@@ -190,14 +190,14 @@ class TestDiversityRecommenders(unittest.TestCase):
 
     def test_intra_list_diversity_usage(self):
         metric = DiversityRecoMetrics.IntraListDiversity(self.item_features, click_column='score', k=4)
-        results = metric.get_score(self.predicted, batch_accumulate=False, return_extended_results=True)
+        results = metric.get_score(self.actual, self.predicted, batch_accumulate=False, return_extended_results=True)
 
         self.assertEqual(np.round(results['intra-list diversity'], 3), 0.267)
         self.assertEqual(results['support'], 3)
 
     def test_intra_list_diversity_sample_default(self):
         metric = DiversityRecoMetrics.IntraListDiversity(self.item_features, click_column='score', k=4)
-        results = metric.get_score(self.predicted, batch_accumulate=False,
+        results = metric.get_score(self.actual, self.predicted, batch_accumulate=False,
                                    return_extended_results=True)
 
         self.assertEqual(0.267, np.round(results['intra-list diversity'], 3))
@@ -206,21 +206,25 @@ class TestDiversityRecommenders(unittest.TestCase):
     def test_intra_list_diversity_sample_given(self):
         metric = DiversityRecoMetrics.IntraListDiversity(self.item_features, click_column='score', k=4,
                                                          user_sample_size=2)
-        results = metric.get_score(self.predicted, batch_accumulate=False,
+        results = metric.get_score(self.actual, self.predicted, batch_accumulate=False,
                                    return_extended_results=True)
 
         self.assertEqual(0.268, np.round(results['intra-list diversity'], 3))
         self.assertEqual(2, results['support'])
 
     def test_intra_list_diversity_sample_one_item(self):
-        metric = DiversityRecoMetrics.IntraListDiversity(self.item_features, click_column='score', k=1)
-        results = metric.get_score(self.predicted, batch_accumulate=False,
+        with warnings.catch_warnings():
+          warnings.simplefilter("ignore")
+          metric = DiversityRecoMetrics.IntraListDiversity(self.item_features, click_column='score', k=1)
+          results = metric.get_score(self.actual, self.predicted, batch_accumulate=False,
                                    return_extended_results=True)
 
-        self.assertTrue(np.isnan(results['intra-list diversity']))
-        self.assertEqual(3, results['support'])
+          self.assertEqual(None, results)
 
     def test_intra_list_diversity_one(self):
+        actual = pd.DataFrame({Constants.user_id: [0, 0],
+                                  Constants.item_id: [0, 1],
+                                  'score': [1, 1]})
         predicted = pd.DataFrame({Constants.user_id: [0, 0],
                                   Constants.item_id: [0, 1],
                                   'score': [1, 1]})
@@ -228,7 +232,7 @@ class TestDiversityRecommenders(unittest.TestCase):
                                       'feat1': [0, 1]})
 
         metric = DiversityRecoMetrics.IntraListDiversity(item_features, click_column='score', k=2)
-        results = metric.get_score(predicted, batch_accumulate=False,
+        results = metric.get_score(actual, predicted, batch_accumulate=False,
                                    return_extended_results=True)
 
         self.assertEqual(results['intra-list diversity'], 1)
@@ -242,22 +246,19 @@ class TestDiversityRecommenders(unittest.TestCase):
     def test_intra_list_diversity_sample_float(self):
         metric = DiversityRecoMetrics.IntraListDiversity(self.item_features, click_column='score', k=4,
                                                          user_sample_size=0.8)
-        results = metric.get_score(self.predicted, batch_accumulate=False, return_extended_results=True)
+        results = metric.get_score(self.actual, self.predicted, batch_accumulate=False, return_extended_results=True)
 
         self.assertEqual(0.268, np.round(results['intra-list diversity'], 3))
         self.assertEqual(2, results['support'])
 
     def test_intra_list_diversity_batch_accumulate_true(self):
         metric = DiversityRecoMetrics.IntraListDiversity(self.item_features, click_column='score', k=4)
-
-        with self.assertRaises(ValueError):
-            # This should fail when `batch_accumulate=True`.
-            results = metric.get_score(self.predicted, batch_accumulate=True, return_extended_results=True)
+        results = metric.get_score(self.actual, self.predicted, batch_accumulate=True, return_extended_results=True)
 
     def test_intra_list_diversity_metric_euclidean(self):
         metric = DiversityRecoMetrics.IntraListDiversity(self.item_features, click_column='score', k=4,
                                                          metric='euclidean')
-        results = metric.get_score(self.predicted, batch_accumulate=False, return_extended_results=True)
+        results = metric.get_score(self.actual, self.predicted, batch_accumulate=False, return_extended_results=True)
 
         self.assertEqual(round(results['intra-list diversity'], 3), 1.307)
         self.assertEqual(results['support'], 3)
